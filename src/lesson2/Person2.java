7b67b1e18c3907f6ecc17f9eba88eb7e577048ff
dmytro.tymoshenko <tymoshenkodmi@gmail.com>
2019-02-25 18:41:52 +0200

    msg

--- src/lesson2/Person2.java
+++ src/lesson2/Person2.java
@@ -1,9 +1,17 @@
 package lesson2;
 
 public class Person2 {
-    public static void main (String[] args) {
-        Person person = new Person("","",5);
-        person.doSomething();
+    public static void main(String[] args) {
+
+        WorkWithString test = new WorkWithString();
+        String t = test.eachNth("fufjufnfifn", 2);
+
+        System.out.println();
+        System.out.println(t);
+
+        System.out.println(WorkWithString.eachNth("Mirace",2));
+        System.out.println(WorkWithString.eachNth("abcdefg",2));
+        System.out.println(WorkWithString.eachNth("abcdefg",3));
 
     }
 }
